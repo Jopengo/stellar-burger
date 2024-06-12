2d3255f4fc48801063c0e00ac4912cb0d63e316f
Said Isaev <isaiddin@yandex.ru>
2024-06-12 18:32:37 +0300

    test review

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,17 @@
+import { useState, ChangeEvent } from 'react';
+
+export function useForm<T>(inputValues: T) {
+  const [values, setValues] = useState(inputValues);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  return {
+    values,
+    handleChange,
+    setValues,
+    reset: () => setValues(inputValues)
+  };
+}
