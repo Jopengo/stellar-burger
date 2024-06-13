052db89b9d7b7ede2c2c453d6d2b59b4ed9c80a1
Said Isaev <isaiddin@yandex.ru>
2024-06-13 13:07:57 +0300

    test review

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,20 +1,17 @@
 import { useState, ChangeEvent } from 'react';
 
-export function useForm<T>(initialValues: T) {
-  const [values, setValues] = useState<T>(initialValues);
+export function useForm<T>(inputValues: T) {
+  const [values, setValues] = useState(inputValues);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-    const { name, value, type } = event.target;
-    const isCheckbox = (event.target as HTMLInputElement).type === 'checkbox';
-    const checked = (event.target as HTMLInputElement).checked;
-
-    setValues({
-      ...values,
-      [name]: isCheckbox ? checked : value,
-    });
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    setValues({ ...values, [name]: value });
   };
 
-  const reset = () => setValues(initialValues);
-
-  return { values, handleChange, setValues, reset };
+  return {
+    values,
+    handleChange,
+    setValues,
+    reset: () => setValues(inputValues)
+  };
 }
